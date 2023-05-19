2ab1e0d14bbe79cac4a385cda7642f416b3bb56e
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-05-19 00:26:15 -0300

    footer

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,10 @@ const logo_img = document.querySelector("img.logo")
 const theme_button = document.querySelector("img.button-img")
 const body = document.querySelector("body")
 
+const linkedin = document.querySelector("img#icon1")
+const github = document.querySelector("img#icon2")
+const instagram = document.querySelector("img#icon3")
+
 function trocar_tema(){
     const themeIsDark = body.classList.contains("dark")
 
@@ -10,8 +14,16 @@ function trocar_tema(){
     if (themeIsDark){
         theme_button.setAttribute("src", "./src/images/sun-stone.png")
         logo_img.setAttribute("src", "./src/images/pokeball.jpg")
+
+        linkedin.setAttribute("src", "./src/images/icons/social-medias/light/linkedin.png")
+        github.setAttribute("src", "./src/images/icons/social-medias/light/github.png")
+        instagram.setAttribute("src", "./src/images/icons/social-medias/light/instagram.png")
     } else {
         theme_button.setAttribute("src", "./src/images/moon-stone.png")
         logo_img.setAttribute("src", "./src/images/masterball.jpg")
+
+        linkedin.setAttribute("src", "./src/images/icons/social-medias/dark/linkedin.png")
+        github.setAttribute("src", "./src/images/icons/social-medias/dark/github.png")
+        instagram.setAttribute("src", "./src/images/icons/social-medias/dark/instagram.png")
     }
 }
